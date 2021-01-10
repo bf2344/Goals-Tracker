cf56767428ede848890e527923a53cd9f95f4c4f
bf2344 <bford2344@gmail.com>
2021-01-10 06:32:23 -0700

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,15 @@ function App () {
         return (
             <div className="App">
 
-                {/* <LoginButton />
-                <LogoutButton /> */}
-                <ListWidgetContainer href="http://localhost:3001/stats/top" heading="All Users Goals" rowspan={3} />
-                <NumberWidgetContainer href="http://localhost:3001/goals/open" heading="Active Goals" />
-                <GraphWidgetContainer href="http://localhost:3001/goals/progression" heading="Goals Achieved Over Time" colspan={2} rowspan={2} />
-                <NumberWidgetContainer href="http://localhost:3001/goals/today" heading="Today's Goals" />
-                <NumberWidgetContainer href="http://localhost:3001/goals/urgent" heading="'Urgent' Goals" />
-                <NumberWidgetContainer href="http://localhost:3001/goals/response" heading="7 Day Goal Tracker" />
-                <NumberWidgetContainer href="http://localhost:3001/goals/solved" heading="1 Month Goal Tracker" />
+                <LoginButton />
+                <LogoutButton />
+                <ListWidgetContainer href="http://localhost:3001/api/stats/top" heading="All Users Goals" rowspan={3} />
+                <NumberWidgetContainer href="http://localhost:3001/api/goals/open" heading="Active Goals" />
+                <GraphWidgetContainer href="http://localhost:3001/api/goals/progression" heading="Goals Achieved Over Time" colspan={2} rowspan={2} />
+                <NumberWidgetContainer href="http://localhost:3001/api/goals/today" heading="Today's Goals" />
+                <NumberWidgetContainer href="http://localhost:3001/api/goals/urgent" heading="'Urgent' Goals" />
+                <NumberWidgetContainer href="http://localhost:3001/api/goals/response" heading="7 Day Goal Tracker" />
+                <NumberWidgetContainer href="http://localhost:3001/api/goals/solved" heading="1 Month Goal Tracker" />
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css"
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="bf2344.us.auth0.com"
+    clientId="2kTSNwHhvrnK62M22s3YMGiilT25x9HP"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
+
+// create goals page, CSS to make each goal section expand the page on click 
+
+// models - user, goals - progress
+
+//
